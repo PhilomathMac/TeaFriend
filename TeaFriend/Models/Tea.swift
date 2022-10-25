4b68e6baf6f4384d5f9c1e2e71fe5743fff88a3d
KenzieMac <kenziemacd@icloud.com>
2022-10-24 17:55:46 -0700

    Add Tea model

---
+++ TeaFriend/Models/Tea.swift
@@ -0,0 +1,18 @@
+//
+//  Tea.swift
+//  TeaFriend
+//
+//  Created by McKenzie Macdonald on 10/24/22.
+//
+
+import Foundation
+
+struct Tea {
+    var id = UUID()
+    var name: String
+    var description: String
+    var type: TeaType
+    var format: TeaFormat
+    var notes: String
+    var rating: Int
+}
