f5c5a4c6dc74be2075e321496d1342233957354b
McKenzie Macdonald <kenziemacd@icloud.com>
2022-11-17 13:49:30 -0800

    Layout initial FilterByRatingView

--- TeaFriend/Views/ListView/Filters/FilterByRatingView.swift
+++ TeaFriend/Views/ListView/Filters/FilterByRatingView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct FilterByRatingView: View {
+    @State var chosenRating: Int
+    var onColor = Color(.systemGreen)
+    var offColor = Color(.systemGray)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text("Minimum Rating:")
+                .font(.callout)
+                .bold()
+                .padding(.vertical, 5)
+            Spacer()
+            ForEach(0 ..< 5, id: \.self) { num in
+                Button {
+                    chosenRating = num + 1
+                } label: {
+                    Image(systemName: "star.fill")
+                        .foregroundColor(num < chosenRating ? onColor : offColor)
+                }
+
+            }
+        }
+        .padding()
     }
 }
 
 struct FilterByRatingView_Previews: PreviewProvider {
     static var previews: some View {
-        FilterByRatingView()
+        FilterByRatingView(chosenRating: 3)
     }
 }
